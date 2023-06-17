d62dbf2cdc300ffac88368dbee9c86c7daf6b720
SazanDawadi <sajandawadi5@gmail.com>
2023-06-17 12:43:07 +0545

    initial-setup-done

--- basicwebscraper/settings.py
+++ basicwebscraper/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+
+    'firstapp.apps.FirstappConfig',
+    
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
